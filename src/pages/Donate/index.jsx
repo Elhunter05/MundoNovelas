8d5615ec4ed9892778a1a6a96f2b1ce5f9aceb24
Andrés Galván <91505260+Elhunter05@users.noreply.github.com>
2022-03-21 22:26:13 -0300

    Add files via upload

--- src/pages/Donate/index.jsx
+++ src/pages/Donate/index.jsx
@@ -1,18 +1,10 @@
 import Donate from "../../components/Donate";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
 
 const DonatePage = () => {
   return (
     <div>
-      <section className="header">
-        <Header />
-      </section>
       <section className="donate">
         <Donate />
-      </section>
-      <section className="footer">
-        <Footer />
       </section>
     </div>
   );
