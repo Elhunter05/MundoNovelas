43303a9786a2222e016beb639e13db8323f59783
Andrés Galván <91505260+Elhunter05@users.noreply.github.com>
2022-03-21 22:26:56 -0300

    Add files via upload

--- src/pages/Novel/index.jsx
+++ src/pages/Novel/index.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
-import Header from "../../components/Header";
+
 import Chapters from "../../components/Chapters";
-import Footer from "../../components/Footer";
+
 import Novel from "../../components/Novel";
 import novels from "../../services/novels.json";
 
@@ -11,9 +11,6 @@ const NovelPage = () => {
 
   return (
     <div>
-      <section className="header">
-        <Header />
-      </section>
       <section className="novel-chapters">
         <Novel {...novel} />
         <h3>Lista de capítulos:</h3>
@@ -22,9 +19,6 @@ const NovelPage = () => {
           link={novel.link}
           chapters={novel.chapters}
         />
-      </section>
-      <section className="footer">
-        <Footer />
       </section>
     </div>
   );
