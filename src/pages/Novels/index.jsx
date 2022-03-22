6bfe1393c04bdbd8f670638df14be929da4ae5c3
Andrés Galván <91505260+Elhunter05@users.noreply.github.com>
2022-03-21 22:27:18 -0300

    Add files via upload

--- src/pages/Novels/index.jsx
+++ src/pages/Novels/index.jsx
@@ -1,21 +1,13 @@
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
 import Novel from "../../components/Novel";
 import novels from "../../services/novels.json";
 
 const NovelsPage = () => {
   return (
     <div className="novels-container">
-      <section className="header">
-        <Header />
-      </section>
       <section className="novels-main">
         {novels.map((novel) => {
           return <Novel novelStyle="col-6" key={novel.id} {...novel} />;
         })}
-      </section>
-      <section className="footer">
-        <Footer />
       </section>
     </div>
   );
