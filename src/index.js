1e8cdab3a4ee95e2fe97087237e548aeb7e7a026
Andrés Galván <91505260+Elhunter05@users.noreply.github.com>
2022-03-21 22:28:30 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,10 +9,13 @@ import DonatePage from "./pages/Donate";
 import NovelsPage from "./pages/Novels";
 import NovelPage from "./pages/Novel";
 import ChapterPage from "./pages/Chapter";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route
@@ -25,6 +28,7 @@ ReactDOM.render(
         <Route path="/recommendations" element={<RecommendationsPage />} />
         <Route path="/donate" element={<DonatePage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
