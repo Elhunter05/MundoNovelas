097ae79f8f7a712431a38ff4ba52743f15b9dbd1
Andrés Galván <91505260+Elhunter05@users.noreply.github.com>
2022-03-21 22:27:37 -0300

    Add files via upload

--- src/pages/Recommendations/index.jsx
+++ src/pages/Recommendations/index.jsx
@@ -1,19 +1,11 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 import Recommendations from "../../components/AddNovel";
 import "../../assets/recommendations.css";
 
 const RecommendationsPage = () => {
   return (
     <div>
-      <section className="header">
-        <Header />
-      </section>
       <section className="recommendations">
         <Recommendations />
-      </section>
-      <section className="footer">
-        <Footer />
       </section>
     </div>
   );
